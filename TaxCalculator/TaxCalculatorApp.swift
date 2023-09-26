d19eeda33ca8878c4db9d82234ba95e0c443a73f
emioiso <jijjjiijiijijiji@gmail.com>
2023-09-26 18:03:54 +0900

    Initial Commit

---
+++ TaxCalculator/TaxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by emi oiso on 2023/09/26.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
